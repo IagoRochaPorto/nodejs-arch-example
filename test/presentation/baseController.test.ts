bd66aa513459d0b3e76e30d28a67d7f66d25bcd3
Iago Rocha Porto <iagorochaporto@gmail.com>
2023-04-04 11:58:24 -0300

    test: add baseController tests

---
+++ test/presentation/baseController.test.ts
@@ -0,0 +1,25 @@
+import { BaseController } from "./../../src/presentation/http/controllers/baseController"
+
+function makeSut() {
+  class Sut extends BaseController {
+    async perform(req: any, res: any): Promise<any> {
+      return {
+        statusCode: 200,
+        body: { id: 1 }
+      }
+    }
+  }
+  const sut = new Sut()
+  return { sut }
+}
+
+describe('BaseController', () => {
+  it('should call perform method with correct values', async () => {
+    const { sut } = makeSut()
+    const performSpy = jest.spyOn(sut, 'perform')
+
+    await sut.execute({ body: { name: 'any_name' } }, {} as any)
+
+    expect(performSpy).toHaveBeenCalledWith({ body: { name: 'any_name' } }, {} as any)
+  })
+})
