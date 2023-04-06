f5775b373a0b3d2898a9176b76fabb5899a05f01
Iago Rocha Porto <iagorochaporto@gmail.com>
2023-04-06 19:02:39 -0300

    feat: add addUserController

--- src/presentation/http/controllers/user/addUserController.ts
+++ src/presentation/http/controllers/user/addUserController.ts
@@ -1,36 +1,36 @@
 import { HttpRequest, HttpResponse } from "@/presentation/http/protocols";
-import { BaseController } from "../baseController";
 import { AddUser } from "@/modules/user/useCases";
+import { BaseController } from "../baseController";
 
 interface UserBody {
-    name: string
-    email: string
-    password: string
-    age: number
+  name: string
+  email: string
+  password: string
+  age: number
 }
 
 export class AddUserController extends BaseController {
-    constructor(private readonly addUserService: AddUser) {
-        super()
+  constructor(private readonly addUserService: AddUser) {
+    super()
+  }
+
+  protected async perform(req: HttpRequest<UserBody>, res: HttpResponse): Promise<HttpResponse> {
+    const { body } = req
+
+    if (!body) {
+      throw new Error('Should have a body')
     }
 
-    protected async perform(req: HttpRequest<UserBody>, res: Response): Promise<HttpResponse> {
-        const { body } = req
+    const savedUser = await this.addUserService.execute({
+      age: body.age,
+      email: body.email,
+      name: body.name,
+      password: body.password
+    })
 
-        if (!body) {
-            throw new Error('Should have a body')
-        }
-
-        const savedUser = await this.addUserService.execute({
-            age: body.age,
-            email: body.email,
-            name: body.name,
-            password: body.password
-        })
-
-        return {
-            statusCode: 201,
-            body: { id: savedUser.id }
-        }
+    return {
+      statusCode: 201,
+      body: { id: savedUser.id }
     }
+  }
 }
